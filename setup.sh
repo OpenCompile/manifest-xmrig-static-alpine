efb17543a2978481f9d27744a65991f56b5af972
Daniel Hejduk <156322098+danielhejduk@users.noreply.github.com>
2024-04-08 09:26:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,3 @@ set -xe
 apk update
 
 apk add git make cmake libstdc++ gcc g++ automake libtool autoconf linux-headers alpine-sdk
-
-git clone https://github.com/xmrig/xmrig.git -b $VERSION
-
-mkdir xmrig/build && cd xmrig/scripts
-
-./build_deps.sh && cd ../build
-
-cmake .. -DXMRIG_DEPS=scripts/deps -DBUILD_STATIC=ON
-
-make -j$(nproc)
